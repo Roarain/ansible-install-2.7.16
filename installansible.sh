19dfff0ead0e1c7a29d5a5068d0971d1e0de568f
王啸雨 <wangxiaoyu@sensoro.com>
2021-04-20 17:12:00 +0800

    --trusted-host mirrors.aliyun.com

--- installansible.sh
+++ installansible.sh
@@ -4,4 +4,4 @@ set -o errexit
 yum install -y git sshpass
 python get-pip.py
 pip install --upgrade pip
-pip install -r requirements.txt -i http://mirrors.aliyun.com/pypi/simple/
+pip install -r requirements.txt -i http://mirrors.aliyun.com/pypi/simple/ --trusted-host mirrors.aliyun.com
